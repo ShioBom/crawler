92743265a6e3461812e51bde23ee62d5c7da6a21
tangyu <yu.tang02@hand-china.com>
2019-07-28 20:38:10 +0800

    Updates

---
+++ precache-manifest.598661d803cea4ae2baf7a66030ecdfb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "091607882b703dfd3ee566c0ebe43b69",
+    "url": "/shiobom.github.io.crawler/index.html"
+  },
+  {
+    "revision": "d0e6b331f04eabb64398",
+    "url": "/shiobom.github.io.crawler/static/css/main.abf77388.chunk.css"
+  },
+  {
+    "revision": "876970e31cc055c0561f",
+    "url": "/shiobom.github.io.crawler/static/js/2.7aa06407.chunk.js"
+  },
+  {
+    "revision": "d0e6b331f04eabb64398",
+    "url": "/shiobom.github.io.crawler/static/js/main.06d37477.chunk.js"
+  },
+  {
+    "revision": "862e97e761391220f58f",
+    "url": "/shiobom.github.io.crawler/static/js/runtime~main.cd0831c5.js"
+  }
+]);
